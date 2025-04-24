d3c5dc67e88fd5d8bd48fc4e037c1d9282f7ff68
Aniket Kulkarni <aniket.kulkarni@dremio.com>
2025-04-24 11:25:54 -0400

    Support for getting lineage description

--- src/dremioai/servers/frameworks/beeai/server.py
+++ src/dremioai/servers/frameworks/beeai/server.py
@@ -1,18 +1,18 @@
-# 
+#
 #  Copyright (C) 2017-2025 Dremio Corporation
-# 
+#
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-# 
+#
 
 from typing import Union, Optional, Annotated, Any, AsyncGenerator, List, Dict
 from beeai_framework.agents.react.agent import ReActAgent, ReActAgentRunOutput
@@ -87,7 +87,8 @@ class ReactAgentWithSession:
             return f"Unknown event: {event.path}, {event.name}"
 
     def observer(self, emmitter: Emitter):
-        emmitter.on("*.*", lambda data, ev: pp(self.process_events(data, ev)))
+        # emmitter.on("*.*", lambda data, ev: pp(self.process_events(data, ev)))
+        pass
 
     async def run(self, prompt: str) -> ReActAgentRunOutput:
         return await self.agent.run(prompt=prompt, execution=self.execution).observe(
